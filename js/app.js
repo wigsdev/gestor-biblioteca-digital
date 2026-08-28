a721e327ff2564189305ea6c56c1d003cfd20ee4
Miriam Huamán Ayala <mariana.h.ha@gmail.com>
2026-08-27 21:27:24 -0500

    feature(T-05): Correcciones Eliminar Libros del Catalogo

--- js/app.js
+++ js/app.js
@@ -185,21 +185,23 @@ const renderizarLibros = (books) => {
 		if(index !== -1){
 			libros.splice(index, 1);
 			renderizarLibros(libros);
+			actualizarEstadisticas();
 		}
+		return;
 	};
 
-	document.getElementById("book-list").addEventListener("click",(event) =>{
+	document.querySelector("#book-list").addEventListener("click",(event) =>{
 	const deleteButton = event.target.closest(".btn-delete");
 
-	if(!deleteButton)
+	if(!deleteButton){
 		return;
+	}
+		
 	const id = Number(deleteButton.dataset.id);
 	deleteBook(id);
 } );
 
-document.addEventListener("DOMContentLoaded", () =>{
-	renderizarLibros(books);
-})
+
 // ── T-06a: Load edit data ──
 
 // ── T-06b: Save edit changes ──
